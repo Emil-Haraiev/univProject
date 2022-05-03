62cfcdf0904aa91a6ebe793b13f2f1ceaa93ffa5
Emil <e.garaev.dg@gmail.com>
2022-05-03 23:33:14 +0300

    created film card

--- src/components/FilmCard/index.js
+++ src/components/FilmCard/index.js
@@ -6,38 +6,33 @@ import './index.css';
 
 const FilmCard = () => {
     return(
-     <div className='card'>
-         <div className='filmCard'>
-             <div className='cardFront'>
-                 <a href='#'>
-                     <img className='cardImg' src={DoctorStrange} alt='Doctor-Strange'/>
-                 </a>
-                 <div className='filmFormat'>2D</div>
-                 <div className='filmAge'>16+</div>
-
-             </div>
-
-             <div className='cardBack'>
-                 <div className='cardName'>Доктор Стрендж у мультивсесвіті божевілля</div>
-                 <div className='filmRating'>
-                     <img className='cardRating' src={imdb} alt='imdb'/>
-                     <div className='filmRatingTo'>4.4</div>
-                     <div className='filmRatingFrom'>/10</div>
+         <div className='card'>
+             <div className='filmCard'>
+                 <div className='cardFront'>
+                     <a href='#'>
+                         <img className='cardImg' src={DoctorStrange} alt='Doctor-Strange'/>
+                     </a>
+                     <div className='filmFormat'>2D</div>
+                     <div className='filmAge'>16+</div>
                  </div>
-                 <div className='filmInfo'>
-                     <div className='genre'>Жанр</div>
-                     <div className='filmGenre'>екшн, фантастика, Marvel, супергерой</div>
-                     <div className='director'>Режисер</div>
-                     <div className='filmDirector'>Сем Реймі</div>
+                 <div className='cardBack'>
+                     <div className='cardName'>Доктор Стрендж у мультивсесвіті божевілля</div>
+                     <div className='filmRating'>
+                         <img className='cardRating' src={imdb} alt='imdb'/>
+                         <div className='filmRatingTo'>4.4</div>
+                         <div className='filmRatingFrom'>/10</div>
+                     </div>
+                     <div className='filmInfo'>
+                         <div className='genre'>Жанр</div>
+                         <div className='filmGenre'>екшн, фантастика, Marvel, супергерой</div>
+                         <div className='director'>Режисер</div>
+                         <div className='filmDirector'>Сем Реймі</div>
+                     </div>
+                     <div className='rental'>З 05 травня 2022</div>
+                     <div className='filmTechs'>Cinetech+</div>
                  </div>
-                 <div className='rental'>З 05 травня 2022</div>
-                 <div className='filmTechs'>Cinetech+</div>
-
              </div>
-
          </div>
-
-     </div>
     )
 }
 
---
+++ src/constants.js
@@ -0,0 +1,28 @@
+import berkut from './assets/berkut.jpg';
+import dambldor from './assets/dambldor.jpg';
+import moribus from './assets/moribus.jpg';
+import sonik from './assets/sonik.jpg';
+import survivor from './assets/survivor.jpg';
+import paliyka from './assets/paliyka.jpg';
+import doctor from './assets/Doctor-Strange.jpg'
+
+
+const cards = [
+    { img:{berkut}, format: '2D', age: '12+', name: 'Захар Беркут', rate: 5.7,
+     genre: 'драма, історія, бойовик, українське', director: 'Ахтем Сеітаблаєв, Джон Вінн', date: '21 квітня - 4 травня', tech: 'Cinetech+'},
+    { img:{dambldor}, format: '2D', age: '3D', name: 'Фантастичні звірі: Таємниці Дамблдора ', rate: 6.5,
+        genre: 'пригоди, фентезі, сімейний', director: 'Девід Єйтс', date: 'з 26 травня 2022', tech: 'Cinetech+ | RE`LUX'},
+    { img:{moribus}, format: '2D', age: '12+', name: 'Морбіус', rate: 5.1,
+        genre: 'фантастика, трилер, пригоди, екшн, супергерой, Marvel', director: 'Даніель Еспіноса', date: 'з 19 травня 2022',tech: 'Cinetech+ | RE`LUX'},
+    { img:{sonik}, format: '2D', age: '12+', name: 'Їжак Сонік 2', rate: 6.9,
+        genre: 'фантастика, пригоди', director: 'Джефф Фоулер', date: 'з 26 травня 2022',tech: 'Cinetech+'},
+    { img:{survivor}, format: '2D', age: '3D', name: 'Гаррі Гафт: той, хто вижив', rate: 6.8,
+        genre: 'драма, історія, біографія', director: 'Баррі Левінсон', date: 'з 26 травня 2022',tech: 'Cinetech+'},
+    { img:{paliyka}, format: '2D', age: '16+', name: 'Палійка ', rate: 5.1,
+        genre: 'містика, трилер', director: 'Кіт Томас', date: ' 12 травня 2022',tech: 'Cinetech+ | 4DX'},
+    { img:{doctor}, format: '2D', age: '12+', name: 'Доктор Стрендж у мультивсесвіті божевілля', rate: 7.9,
+        genre: 'екшн, фантастика, Marvel, супергерой', director: 'Сем Реймі', date: 'з 16 червня 2022',tech: 'Cinetech+ | 4DX | IMAX'}
+]
+
+
+export default cards;
